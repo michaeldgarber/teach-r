c20dd9cab035f9c137e09844f0a8866b2bc20a4e
michaeldgarber <mdg711@gmail.com>
2022-08-08 16:24:06 -0600

    add links to session 4 recordings

--- scripts/0-manage-packages.R
+++ scripts/0-manage-packages.R
@@ -9,9 +9,11 @@ utils::sessionInfo()
 
 #look at search path
 base::search()
+base::searchpaths()
 
 #what packages have been loaded via a namespace?
 base::loadedNamespaces()
+
 
 #all installed packages
 utils::installed.packages()
@@ -45,3 +47,34 @@ sessioninfo::package_info(pkgs = "loaded") #check
 
 #unload a namespace - useful for packages that have not been attached
 #base::unloadNamespace("package_name")
+
+library(here)
+library(ggplot2)
+sessioninfo::package_info(pkgs = "attached")
+
+sessioninfo::package_info(pkgs = "loaded")
+sessioninfo::package_info(pkgs = "installed")
+
+#remove a package
+base::detach("package:here", unload = TRUE)
+base::detach("package:mapview", unload = TRUE)
+sessioninfo::package_info("attached")
+
+remove.packages("sessioninfo")
+remove.packages("sf")
+remove.packages("tidycensus")
+install.packages("tidycensus") #sf would import
+
+install.packages("sessioninfo")
+
+
+packages  = utils::available.packages()
+#View(packages)
+library(tidyverse) #so the pipe works
+packages_tibble_tidycensus = packages %>% 
+  dplyr::as_tibble() %>% 
+  dplyr::filter(Package == "tidycensus")
+
+#Just select these two fields.
+packages_tibble_tidycensus %>% 
+  dplyr::select(Package, Imports)
